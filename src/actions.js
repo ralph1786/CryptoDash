6da160caa174947803ef891a14df9ba2717d866a
ralph1786 <ralph1786@gmail.com>
2019-04-29 13:04:11 -0400

    action creator, fetch info with axios

---
+++ src/actions.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { apiKey } from "./constant";
+
+const loadCurrencies = listCurrencies => ({
+  type: "LOAD_CURRENCIES",
+  payload: listCurrencies
+});
+
+export const allCurrencies = () => {
+  return dispatch => {
+    return axios
+      .get(`https://api.nomics.com/v1/currencies/ticker?key=${apiKey}`)
+      .then(res => {
+        const listCurrencies = res.data.slice(0, 6);
+        dispatch(loadCurrencies(listCurrencies));
+        // debugger;
+      })
+      .catch(err => console.log(err));
+  };
+};
