6c88ada64d9b9a796b0b827d74a5f3630e7ba7b7
ralph1786 <ralph1786@gmail.com>
2019-04-29 13:05:30 -0400

    added react-router, connected to redux

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import currenciesReducer from "./reducer";
+import thunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+const store = createStore(currenciesReducer, enhancer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
