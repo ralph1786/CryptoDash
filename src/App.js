07335ab7835b102ca33e0ff653211b2ffaee3246
ralph1786 <ralph1786@gmail.com>
2019-04-30 10:58:08 -0400

    added new route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./containers/Home";
+import Dashboard from "./containers/Dashboard";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" render={() => <Home />} />
-    </Switch>
+    <Fragment>
+      <Navbar />
+      <Switch>
+        <Route path="/dashboard" render={() => <Dashboard />} />
+        <Route path="/" render={() => <Home />} />
+      </Switch>
+    </Fragment>
   );
 }
 
