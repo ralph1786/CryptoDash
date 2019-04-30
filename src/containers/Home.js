da984d31567f04fcffd94f1661e62e58f5c7b765
ralph1786 <ralph1786@gmail.com>
2019-04-30 17:01:50 -0400

    added video container

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,13 @@
 import React, { Component, Fragment } from "react";
 import CurrencyContainer from "./CurrencyContainer";
+import VideoContainer from "../components/VideoContainer";
 import "./Home.scss";
 
 class Home extends Component {
   render() {
     return (
       <Fragment>
-        <div className="animation">Animation</div>
+        <VideoContainer />
         <CurrencyContainer />
       </Fragment>
     );
