a65f4a562e969440c3398c868e0007bbfa2e4652
ralph1786 <ralph1786@gmail.com>
2019-05-03 10:43:58 -0400

    remoced scss file import

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,7 +1,6 @@
 import React, { Component, Fragment } from "react";
 import CurrencyContainer from "./CurrencyContainer";
 import VideoContainer from "../components/VideoContainer";
-import "./Home.scss";
 
 class Home extends Component {
   render() {
