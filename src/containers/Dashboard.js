43b8e7d9dad799e5c43ac4fe03a5eff3c92618ae
ralph1786 <ralph1786@gmail.com>
2019-05-10 10:12:03 -0400

    added modal component

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
 import { secondApiKey } from "../constant";
@@ -17,6 +16,7 @@ import {
   BarChart,
   Bar
 } from "recharts";
+import ModalContainer from "./ModalContainer";
 
 class Dashboard extends Component {
   state = {
@@ -26,6 +26,16 @@ class Dashboard extends Component {
     start: 0,
     end: 10
   };
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    // console.log(nextProps);
+    if (prevState.chosenCurrency !== nextProps.selectedCurrency.currency) {
+      return {
+        chosenCurrency: nextProps.selectedCurrency.currency
+      };
+    }
+    return null;
+  }
 
   getCurrencyData = () => {
     axios
@@ -58,120 +68,119 @@ class Dashboard extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevState.days !== this.state.days) {
+    if (
+      prevState.days !== this.state.days ||
+      prevState.chosenCurrency !== this.state.chosenCurrency
+    ) {
       this.getCurrencyData();
     }
   }
 
   changeHandler = e => {
-    this.setState(
-      {
-        days: e.target.value
-      },
-      () => console.log(this.state.days)
-    );
+    this.setState({
+      days: e.target.value
+    });
   };
 
   nextArticlesHandler = () => {
     const addStart = this.state.start + 10;
     const addEnd = this.state.end + 10;
-    this.setState(
-      {
-        start: addStart,
-        end: addEnd
-      },
-      () => console.log(this.state)
-    );
+    this.setState({
+      start: addStart,
+      end: addEnd
+    });
   };
 
   prevArticlesHandler = () => {
     const subtractStart = this.state.start - 10;
     const subtractEnd = this.state.end - 10;
-
-    this.setState(
-      {
-        start: subtractStart,
-        end: subtractEnd
-      },
-      () => console.log(this.state)
-    );
+    this.setState({
+      start: subtractStart,
+      end: subtractEnd
+    });
   };
 
   render() {
     return (
-      <div className="dashboard">
-        <GeneralInfo />
-        <SelectMenu value={this.state.days} handleChange={this.changeHandler} />
-        <div className="chart">
-          <LineChart
-            width={800}
-            height={400}
-            data={this.state.data}
-            margin={{
-              top: 10,
-              right: 30,
-              left: 0,
-              bottom: 0
-            }}
-          >
-            <CartesianGrid strokeDasharray="5 5" />
-            <XAxis dataKey="time" />
-            <YAxis />
-            <Tooltip />
-            <Line
-              type="monotone"
-              dataKey="close"
-              stroke="#045ff2"
-              fill="#f90202"
-            />
-          </LineChart>
-        </div>
-        <div className="chart-two">
-          <BarChart
-            width={800}
-            height={400}
-            data={this.state.data}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 10,
-              bottom: 5
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="time" />
-            <YAxis />
-            <Tooltip />
-            <Bar dataKey="close" fill="#5292f9" />
-          </BarChart>
-        </div>
-        <div className="news">
-          <h2>
-            {this.state.start === 0 ? (
-              " "
-            ) : (
-              <i
-                className="fas fa-caret-left"
-                onClick={this.prevArticlesHandler}
+      <Fragment>
+        <div className="dashboard">
+          <GeneralInfo />
+          <SelectMenu
+            value={this.state.days}
+            handleChange={this.changeHandler}
+          />
+          <div className="chart">
+            <LineChart
+              width={800}
+              height={400}
+              data={this.state.data}
+              margin={{
+                top: 10,
+                right: 30,
+                left: 0,
+                bottom: 0
+              }}
+            >
+              <CartesianGrid strokeDasharray="5 5" />
+              <XAxis dataKey="time" />
+              <YAxis />
+              <Tooltip />
+              <Line
+                type="monotone"
+                dataKey="close"
+                stroke="#045ff2"
+                fill="#f90202"
               />
-            )}
-            Latest News
-            {this.state.end === 50 ? (
-              " "
-            ) : (
-              <i
-                className="fas fa-caret-right"
-                onClick={this.nextArticlesHandler}
-              />
-            )}
-          </h2>
-          <NewsContainer info={this.state} />
+            </LineChart>
+          </div>
+          <div className="chart-two">
+            <BarChart
+              width={800}
+              height={400}
+              data={this.state.data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 10,
+                bottom: 5
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="time" />
+              <YAxis />
+              <Tooltip />
+              <Bar dataKey="close" fill="#5292f9" />
+            </BarChart>
+          </div>
+          <div className="news">
+            <h2>
+              {this.state.start === 0 ? (
+                " "
+              ) : (
+                <i
+                  className="fas fa-caret-left"
+                  onClick={this.prevArticlesHandler}
+                />
+              )}
+              Latest News
+              {this.state.end === 50 ? (
+                " "
+              ) : (
+                <i
+                  className="fas fa-caret-right"
+                  onClick={this.nextArticlesHandler}
+                />
+              )}
+            </h2>
+            <NewsContainer info={this.state} />
+          </div>
         </div>
-      </div>
+        <ModalContainer />
+      </Fragment>
     );
   }
 }
 
 const mapStateToProps = state => ({ selectedCurrency: state.selectedCurrency });
 
-export default withRouter(connect(mapStateToProps)(Dashboard));
+export default connect(mapStateToProps)(Dashboard);
