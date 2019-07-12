fcde11e7c3df17c8ef308cd85b848ee0849b827c
ralph1786 <ralph1786@gmail.com>
2019-07-12 12:30:54 -0400

    added graphql

--- src/containers/ModalContainer.js
+++ src/containers/ModalContainer.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Modal from "react-modal";
 import { connect } from "react-redux";
 import { toggleModal } from "../actions";
 import CryptoCard from "../components/CryptoCard";
 import "./ModalContainer.scss";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import { GridLoader } from "react-spinners";
+import { css } from "@emotion/core";
 
 const customStyles = {
   content: {
@@ -16,11 +20,30 @@ const customStyles = {
   }
 };
 
-const ModalContainer = props => {
-  const arrayCurrencies = props.listCurrencies.map((currency, index) => (
-    <CryptoCard key={index} info={currency} />
-  ));
+const override = css`
+  display: block;
+  grid-column: 2/3;
+  margin: 185px auto;
+  border-color: red;
+`;
 
+const CURRENCIES_QUERY = gql`
+  query CurrencyQuery {
+    currencies {
+      currency
+      price
+      logo_url
+      name
+      price_date
+    }
+  }
+`;
+
+const ModalContainer = props => {
+  // const arrayCurrencies = props.listCurrencies.map((currency, index) => (
+  //   <CryptoCard key={index} info={currency} />
+  // ));
+  // console.log(props.listCurrencies);
   return (
     <Modal
       isOpen={props.isModalOpen}
@@ -29,14 +52,39 @@ const ModalContainer = props => {
       ariaHideApp={false}
       closeTimeoutMS={500}
     >
-      <div className="modal-container">{arrayCurrencies}</div>
+      <div className="modal-container">
+        <Query query={CURRENCIES_QUERY} pollInterval={3000}>
+          {({ loading, error, data }) => {
+            if (loading)
+              return (
+                <Fragment>
+                  <GridLoader
+                    css={override}
+                    sizeUnit={"px"}
+                    size={20}
+                    color={"#2f80ed"}
+                  />
+                </Fragment>
+              );
+            if (error) console.log(error);
+            console.log(data);
+            // const slicedData = data.currencies.slice(0, 6);
+            return (
+              <Fragment>
+                {data.currencies.map((currency, index) => (
+                  <CryptoCard key={index} info={currency} />
+                ))}
+              </Fragment>
+            );
+          }}
+        </Query>
+      </div>
     </Modal>
   );
 };
 
 const mapStateToProps = state => {
   return {
-    listCurrencies: state.cryptoCurrencies,
     isModalOpen: state.isModalOpen
   };
 };
--- src/containers/NewsContainer.js
+++ src/containers/NewsContainer.js
@@ -1,27 +1,74 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import NewsCard from "../components/NewsCard";
-import { secondApiKey } from "../constant";
-import axios from "axios";
+// import { secondApiKey } from "../constant";
+// import axios from "axios";
 import "./NewsContainer.scss";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import { GridLoader } from "react-spinners";
+import { css } from "@emotion/core";
+
+const NEWS_QUERY = gql`
+  query NewsQuery {
+    news {
+      title
+      categories
+      url
+    }
+  }
+`;
+const override = css`
+  place-self: center;
+`;
 
 class NewsContainer extends Component {
-  state = {
-    articles: []
-  };
+  // state = {
+  //   articles: []
+  // };
 
-  componentDidMount() {
-    axios
-      .get(
-        `https://min-api.cryptocompare.com/data/v2/news/?lang=EN&api_key=${secondApiKey}`
-      )
-      .then(res => this.setState({ articles: res.data.Data }));
-  }
+  // componentDidMount() {
+  //   axios
+  //     .get(
+  //       `https://min-api.cryptocompare.com/data/v2/news/?lang=EN&api_key=${secondApiKey}`
+  //     )
+  //     .then(res => this.setState({ articles: res.data.Data }));
+  // }
 
   render() {
-    const listOfArticles = this.state.articles
-      .slice(this.props.info.start, this.props.info.end)
-      .map(article => <NewsCard info={article} key={article.id} />);
-    return <div className="news-container">{listOfArticles}</div>;
+    // const listOfArticles = this.state.articles
+    //   .slice(this.props.info.start, this.props.info.end)
+    //   .map(article => <NewsCard info={article} key={article.id} />);
+    return (
+      <div className="news-container">
+        {/* {listOfArticles} */}
+        <Query query={NEWS_QUERY}>
+          {({ loading, error, data }) => {
+            if (loading) {
+              return (
+                <Fragment>
+                  <GridLoader
+                    css={override}
+                    sizeUnit={"px"}
+                    size={20}
+                    color={"#2f80ed"}
+                  />
+                </Fragment>
+              );
+            }
+            if (error) console.log(error);
+            return (
+              <Fragment>
+                {data.news
+                  .slice(this.props.info.start, this.props.info.end)
+                  .map(article => (
+                    <NewsCard info={article} key={article.id} />
+                  ))}
+              </Fragment>
+            );
+          }}
+        </Query>
+      </div>
+    );
   }
 }
 
