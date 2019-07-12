2e4fc11c864c44d4603e69050825915f3fefffcf
ralph1786 <ralph1786@gmail.com>
2019-07-12 09:16:38 -0400

    connected to grapql using apollo client

--- src/containers/CurrencyContainer.js
+++ src/containers/CurrencyContainer.js
@@ -1,38 +1,89 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { allCurrencies } from "../actions";
-import { withRouter } from "react-router-dom";
+import React, { Component, Fragment } from "react";
+// import { connect } from "react-redux";
+// import { allCurrencies } from "../actions";
+// import { withRouter } from "react-router-dom";
 import CryptoCard from "../components/CryptoCard";
 import "./CurrencyContainer.scss";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import { GridLoader } from "react-spinners";
+import { css } from "@emotion/core";
+
+const CURRENCY_QUERY = gql`
+  query CryptoCurrencies {
+    currencies {
+      currency
+      price
+    }
+  }
+`;
+
+const override = css`
+  display: block;
+  grid-column: 2/3;
+  margin: 185px auto;
+  border-color: red;
+`;
 
 class CurrencyContainer extends Component {
-  componentDidMount() {
-    this.props.allCurrencies();
-  }
+  // componentDidMount() {
+  //   this.props.allCurrencies();
+  // }
 
   render() {
-    const listOfCurrencies = this.props.listCurrencies.map(
-      (currency, index) => <CryptoCard key={index} info={currency} />
+    // const listOfCurrencies = this.props.listCurrencies.map(
+    //   (currency, index) => <CryptoCard key={index} info={currency} />
+    // );
+    return (
+      <div className="container">
+        {/* {listOfCurrencies} */}
+        <Query query={CURRENCY_QUERY}>
+          {({ loading, error, data }) => {
+            if (loading)
+              return (
+                <Fragment>
+                  <GridLoader
+                    css={override}
+                    sizeUnit={"px"}
+                    size={20}
+                    color={"#FFF"}
+                  />
+                </Fragment>
+              );
+            if (error) console.log(error);
+
+            const slicedData = data.currencies.slice(0, 6);
+            return (
+              <Fragment>
+                {slicedData.map((currency, index) => (
+                  <CryptoCard key={index} info={currency} />
+                ))}
+              </Fragment>
+            );
+          }}
+        </Query>
+      </div>
     );
-    return <div className="container">{listOfCurrencies}</div>;
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    listCurrencies: state.cryptoCurrencies
-  };
-};
+// const mapStateToProps = state => {
+//   return {
+//     listCurrencies: state.cryptoCurrencies
+//   };
+// };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    allCurrencies: () => dispatch(allCurrencies())
-  };
-};
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     allCurrencies: () => dispatch(allCurrencies())
+//   };
+// };
 
-export default withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(CurrencyContainer)
-);
+// export default withRouter(
+//   connect(
+//     mapStateToProps,
+//     mapDispatchToProps
+//   )(CurrencyContainer)
+// );
+
+export default CurrencyContainer;
--- src/index.js
+++ src/index.js
@@ -9,19 +9,27 @@ import { Provider } from "react-redux";
 // import thunk from "redux-thunk";
 import { store, persistor } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // const enhancer = composeEnhancers(applyMiddleware(thunk));
 
 // const store = createStore(currenciesReducer, enhancer);
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>,
   document.getElementById("root")
 );
