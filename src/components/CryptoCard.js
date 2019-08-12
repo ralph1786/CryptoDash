2b045025c91dc9714155b0cf18125cdd2ca124ed
ralph1786 <ralph1786@gmail.com>
2019-08-12 11:54:21 -0400

    added color change depending on price change

--- src/components/CryptoCard.js
+++ src/components/CryptoCard.js
@@ -1,37 +1,70 @@
-import React from "react";
+import React, { Component } from "react";
 import "./CryptoCard.scss";
 import { Link, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { selectedCurrency } from "../actions";
 // import currencyImage from "./currencyImage";
 
-const CryptoCard = props => {
-  const { currency, price, logo_url, name } = props.info;
-  const priceInt = parseFloat(price).toFixed(4);
-  return (
-    <div
-      className="cryptoCard"
-      onClick={
-        props.location.pathname === "/dashboard"
-          ? () => props.selectedCurrency(props.info)
-          : () => console.log("hello")
+class CryptoCard extends Component {
+  state = {
+    price: this.props.info.price,
+    priceIncrease: false
+  };
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (prevState.price !== nextProps.info.price) {
+      if (prevState.price < nextProps.info.price) {
+        return {
+          price: nextProps.info.price,
+          priceIncrease: true
+        };
+      } else {
+        return {
+          price: nextProps.info.price,
+          priceIncrease: false
+        };
       }
-    >
-      <h3>
-        {name} ({currency})
-      </h3>
-      <img src={logo_url} alt="currency logo" />
-      <span>${priceInt} USD</span>
-      {props.location.pathname === "/dashboard" ? null : (
-        <Link to="/dashboard">
-          <button onClick={() => props.selectedCurrency(props.info)}>
-            Dashboard
-          </button>
-        </Link>
-      )}
-    </div>
-  );
-};
+    }
+    return null;
+  }
+
+  render() {
+    const { currency, logo_url, name } = this.props.info;
+    const priceInt = parseFloat(this.state.price).toFixed(4);
+    // console.log(this.state.priceIncrease);
+    return (
+      <div
+        className="cryptoCard"
+        onClick={
+          this.props.location.pathname === "/dashboard"
+            ? () => this.props.selectedCurrency(this.props.info)
+            : () => console.log("hello")
+        }
+      >
+        <h3>
+          {name} ({currency})
+        </h3>
+        <img src={logo_url} alt="currency logo" />
+        <span
+          style={
+            this.state.priceIncrease ? { color: "green" } : { color: "red" }
+          }
+        >
+          ${priceInt} USD
+        </span>
+        {this.props.location.pathname === "/dashboard" ? null : (
+          <Link to="/dashboard">
+            <button
+              onClick={() => this.props.selectedCurrency(this.props.info)}
+            >
+              Dashboard
+            </button>
+          </Link>
+        )}
+      </div>
+    );
+  }
+}
 
 const mapDispatchToProps = dispatch => {
   return {
