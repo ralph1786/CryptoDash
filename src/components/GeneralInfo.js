b51514c7d4b08222786212900b48983c6c646606
ralph1786 <ralph1786@gmail.com>
2019-08-22 13:28:20 -0400

    added loader

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { connect } from "react-redux";
 import "./GeneralInfo.scss";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
+import { GridLoader } from "react-spinners";
+import { css } from "@emotion/core";
+import ErrorMessage from "../components/ErrorMessage";
 
 const CURRENCY_QUERY = gql`
   query CurrencyQuery($currency: String!) {
@@ -16,6 +19,12 @@ const CURRENCY_QUERY = gql`
   }
 `;
 
+const override = css`
+  display: block;
+  grid-column: 1/2;
+  margin: 100px auto;
+`;
+
 const GeneralInfo = props => {
   const { currency } = props.chosenCurrency;
 
@@ -23,9 +32,21 @@ const GeneralInfo = props => {
     <Query query={CURRENCY_QUERY} variables={{ currency }} pollInterval={3000}>
       {({ loading, error, data }) => {
         if (loading) {
-          return <h2>Data being retrieved...</h2>;
+          return (
+            <Fragment>
+              <GridLoader
+                css={override}
+                sizeUnit={"px"}
+                size={20}
+                color={"#007aff"}
+              />
+            </Fragment>
+          );
         }
-        if (error) console.log(error);
+        if (error) {
+          console.log(error);
+          return <ErrorMessage />;
+        }
         const { logo_url, name, currency, price, price_date } = data.currency;
         return (
           <div className="basic-info">
