7625103520ad13eeef9151810ba42214448b1786
ralph1786 <ralph1786@gmail.com>
2019-08-22 09:54:38 -0400

    added conditional for styling and icon

--- src/components/CryptoCard.js
+++ src/components/CryptoCard.js
@@ -7,7 +7,8 @@ import { selectedCurrency } from "../actions";
 class CryptoCard extends Component {
   state = {
     price: this.props.info.price,
-    priceIncrease: false
+    priceIncrease: false,
+    priceDecrease: false
   };
 
   static getDerivedStateFromProps(nextProps, prevState) {
@@ -15,17 +16,47 @@ class CryptoCard extends Component {
       if (prevState.price < nextProps.info.price) {
         return {
           price: nextProps.info.price,
-          priceIncrease: true
+          priceIncrease: true,
+          priceDecrease: false
         };
       } else {
         return {
           price: nextProps.info.price,
-          priceIncrease: false
+          priceIncrease: false,
+          priceDecrease: true
         };
       }
     }
     return null;
   }
+
+  priceStyling = () => {
+    const { priceIncrease, priceDecrease } = this.state;
+    if (!priceIncrease && !priceDecrease) {
+      return {
+        color: "black"
+      };
+    } else if (priceIncrease && !priceDecrease) {
+      return {
+        color: "green"
+      };
+    } else if (!priceIncrease && priceDecrease) {
+      return {
+        color: "red"
+      };
+    }
+  };
+
+  showIcon = () => {
+    const { priceDecrease, priceIncrease } = this.state;
+    if (!priceDecrease && !priceIncrease) {
+      return null;
+    } else if (priceIncrease) {
+      return <i className="fas fa-caret-up fa-lg" />;
+    } else if (priceDecrease) {
+      return <i className="fas fa-caret-down fa-lg" />;
+    }
+  };
 
   render() {
     const { currency, logo_url, name } = this.props.info;
@@ -43,17 +74,8 @@ class CryptoCard extends Component {
           {name} ({currency})
         </h3>
         <img src={logo_url} alt="currency logo" />
-        <span
-          style={
-            this.state.priceIncrease ? { color: "green" } : { color: "red" }
-          }
-        >
-          ${priceInt} USD{" "}
-          {this.state.priceIncrease ? (
-            <i className="fas fa-caret-up fa-lg" />
-          ) : (
-            <i className="fas fa-caret-down fa-lg" />
-          )}
+        <span style={this.priceStyling()}>
+          ${priceInt} USD {this.showIcon()}
         </span>
         {this.props.location.pathname === "/dashboard" ? null : (
           <Link to="/dashboard">
